06591326a7cad5d7c07227fd1fd62a91acddcd07
Esmee <esmeejanssen99@gmail.com>
2019-12-13 10:42:51 +0100

    Form met javascript af en grid toegepast
    Ik heb de javascript regels toepast aan de filter button en de filterbutton plus filterblok een transision gegeven

--- _js/script.js
+++ _js/script.js
@@ -1,3 +1,12 @@
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
 /*eslint-env browser*/
 /*eslint 'no-console':0*/
+
+var filteren = document.querySelector("#filterbutton");
+
+function filterShow() {
+    var inUitKlappen = document.querySelector("#filterblok");
+    inUitKlappen.classList.toggle("show");
+}
+
+filteren.addEventListener('click', filterShow)
